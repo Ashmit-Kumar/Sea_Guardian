164ae8f0b8b89db3d80497ce6ab0a6cf974c1804
Ashmit Kumar <ashmitkumar1020@gmail.com>
2024-09-20 22:08:53 +0530

    Add Files to repo

---
+++ Seaguardian_ServerSide/db/connect.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const fs = require('fs');
+
+const connectDB = async (databaseName) => {
+  try {
+    const db_url = process.env.MONGODB_URL;
+    await mongoose.connect(db_url, {
+      dbName:databaseName
+    });
+    console.log('MongoDB connected');
+    console.log(mongoose.connect(db_url));
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+  }
+};
+// connectDB()
+module.exports = { connectDB };
